55b486f3e310c3d44cc519edf2c460b02a68bc82
Kizito Azubuike <kayzubi84@gmail.com>
2022-02-27 00:25:49 +0100

    Complete mobile view

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Variable declarations
 const toggleBtn = document.querySelector('.toggle-btn');
+const collapseNavBar = document.querySelector('.navbar-collapse');
 
 
 
@@ -10,12 +11,17 @@ toggleBtn.addEventListener('click', showNav);
 
 function showNav(e) {
     
+
+    //change toggle button icon
     if(toggleBtn.firstElementChild.classList.contains('fa-bars')) {
         toggleBtn.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+        collapseNavBar.classList.replace('m-fadeOut', 'm-fadeIn');
+        document.body.classList.add('overflow');
     } else if (toggleBtn.firstElementChild.classList.contains('fa-xmark')) {
         toggleBtn.innerHTML =`<i class="fa-solid fa-bars"></i>`;
+        collapseNavBar.classList.replace('m-fadeIn', 'm-fadeOut');
+        document.body.classList.remove('overflow');
     }
-
 
     e.prventDefault();
 }
